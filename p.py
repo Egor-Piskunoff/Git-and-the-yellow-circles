8973828ec3aebeb836edc97579c7dfab14d7cb9a
Egor-Piskunoff <egork.piskunoff@yandex.com>
2025-02-10 17:44:57 +0300

    update

--- p.py
+++ p.py
@@ -1,14 +1,44 @@
 import sys
 from random import randint
-from PyQt6 import uic
+from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtWidgets import QMainWindow, QApplication
-from PyQt6.QtGui import QPainter, QColor, QPaintEvent
+from PyQt6.QtGui import QFont, QPaintEvent, QPainter, QColor
 
 
-class MainWindow(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(240, 510, 281, 41))
+        font = QFont()
+        font.setPointSize(18)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Git и желтые окружности"))
+        self.pushButton.setText(_translate("MainWindow", "Нарисовать окружности"))
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('untitled1.ui', self)
+        self.setupUi(self)
         self.initUi()
 
     def initUi(self):
@@ -27,8 +57,7 @@ class MainWindow(QMainWindow):
             qp.end()
 
     def draw(self, qp: QPainter):
-        qp.setPen(QColor('yellow'))
-        qp.setBrush(QColor('yellow'))
+        colors = [QColor(randint(0, 255), randint(0, 255), randint(0, 255)) for i in range(3)]
         circles = []
         while len(circles) < 3:
             radius = randint(10, 80)
@@ -36,6 +65,9 @@ class MainWindow(QMainWindow):
             y = randint(0 + radius, self.centralwidget.height() - radius)
             if not any(self.not_overlay(x, y, radius, circle) for circle in
                        circles):
+                color = colors[len(circles)]
+                qp.setPen(color)
+                qp.setBrush(color)
                 circles.append((x, y, radius))
                 qp.drawEllipse(x - radius, y - radius, radius * 2, radius * 2)
 
@@ -43,7 +75,6 @@ class MainWindow(QMainWindow):
         circle_x, circle_y, circle_radius = circle
         distance = (circle_x - x) ** 2 + (circle_y - y) ** 2
         return distance < (circle_radius + radius) ** 2
-
 
 
 if __name__ == '__main__':
